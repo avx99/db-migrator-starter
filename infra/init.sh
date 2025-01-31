c4d7b230c95adf6eac8073c189c549e2bde441f3
avx99 <abz1999abz@gmail.com>
2025-01-31 01:51:26 +0100

    init

--- infra/init.sh
+++ infra/init.sh
@@ -16,7 +16,4 @@ kubectl apply -f old-db/
 echo "Applying New Database configurations..."
 kubectl apply -f new-db/
 
-echo "Applying Identity Provider configurations..."
-kubectl apply -f idp/
-
 echo "All configurations have been applied successfully!"
